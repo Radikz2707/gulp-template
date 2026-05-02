fd448bb34c49286e4c866f8324b27a4bdaff4a67
Radik <Radikz2707@yandex.ru>
2026-05-02 14:51:11 +0300

    Настроил импорты через алиасы

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,11 @@
-import { header } from "../components/header/header.js";
-import { main } from "../components/main/main.js";
-import { footer } from "../components/footer/footer.js";
-import { isWebp } from "./modules/isWebp.js";
-
+import { header } from "@/../components/header/header.js";
+import { main } from "@/../components/main/main.js";
+import { footer } from "@/../components/footer/footer.js";
+import { isWebp } from "@/modules/isWebp.js"; // Намного чище!
 
 isWebp();
 header();
 main();
 footer();
 
-console.log("Gulp работает, скрипты собраны!");
-
+console.log("Gulp работает, алиасы настроены!");
